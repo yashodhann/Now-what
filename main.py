b9839b813d3aa0076af3a1fde36578540125417e
Yashodhan Pagar <yashodhansharadpagar@gmail.com>
2025-06-20 11:55:14 +0530

    Bug Fixes

--- main.py
+++ main.py
@@ -96,5 +96,6 @@ def update_task(task_id):
     db.session.commit()
     return jsonify(task.to_dict())
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=10000)
